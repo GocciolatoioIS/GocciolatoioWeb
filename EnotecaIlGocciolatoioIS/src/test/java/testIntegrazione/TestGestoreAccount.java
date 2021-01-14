9ca38f8ff04325296cc085ac066a3e4bc58889a2
TheDMG8 <44084060+TheDMG8@users.noreply.github.com>
2021-01-14 17:50:49 +0100

    TestIntegrazione

---
+++ EnotecaIlGocciolatoioIS/src/test/java/testIntegrazione/TestGestoreAccount.java
@@ -0,0 +1,54 @@
+package testIntegrazione;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import model.GestoreAccount;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+public class TestGestoreAccount extends Mockito{
+
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    public final GestoreAccount gestoreAccount = new GestoreAccount();
+
+    @BeforeEach
+    void setUp() throws Exception {
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+        MockitoAnnotations.initMocks(this);
+
+    }
+
+    @Test
+    void TC_Utente() throws ServletException, IOException {
+
+        //aggiungere ai request i parametri
+        request.addParameter("nome","Giovanni");
+        request.addParameter("cognome","");
+
+        String message = "il cognome non è vuoto";
+
+        gestoreAccount.gestioneRegistrazione(request, response);
+        String result = (String) request.getAttribute("errorTest");
+        assertEquals(message, result);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        request=null;
+        response=null;
+    }
+
+}
